a64246bf16ee3edf48d0983f4bdc0b516be20b15
james-antonio <134082404+james-antonio@users.noreply.github.com>
2023-05-19 19:21:40 +0000

    Update Sports.py

--- Sports.py
+++ Sports.py
@@ -15,7 +15,7 @@ image = Image.open('SPORTS.jpg')
 st.image(image, caption='Sports Celebrity Classifier')
 
 with st.container():
-    col1, col2, col3 = st.columns((40,50,40))
+    col1, col2, col3 = st.columns((10,50,10))
     image = Image.open('SPORTS.jpg')
     st.image(image, caption='Sports Celebrity Classifier')
 
