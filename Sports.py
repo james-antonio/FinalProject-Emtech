6f416194adacd11ef9421dd57ea6308b39eaf27a
james-antonio <134082404+james-antonio@users.noreply.github.com>
2023-05-19 19:07:48 +0000

    Update Sports.py

--- Sports.py
+++ Sports.py
@@ -33,5 +33,5 @@ else:
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
     class_names=['Kane Williamson','Kobe Bryant','Maria Sharapova', 'Ronaldo']
-    string="The picture shown above is a : "+class_names[np.argmax(prediction)]
+    string="Sport Celebrity : "+class_names[np.argmax(prediction)]
     st.success(string)
